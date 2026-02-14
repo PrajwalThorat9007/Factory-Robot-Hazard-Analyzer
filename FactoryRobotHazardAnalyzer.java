25ee2cc686da975bd472c83832a0905d9aea7486
PrajwalThorat9007 <prajwalthorat9007@gmail.com>
2026-02-14 15:00:01 +0530

    [Prajwal]:UC1:Added static system startup message

--- FactoryRobotHazardAnalyzer.java
+++ FactoryRobotHazardAnalyzer.java
@@ -1,2 +1,13 @@
-public class FactoryRobotHazardAnalyzer git {
+// UC1: Display Static Hazard Message
+// Author: Prajwal
+
+public class FactoryRobotHazardAnalyzer {
+
+    // Program entry point
+    public static void main(String[] args) {
+
+        // Displaying static system message
+        System.out.println("Factory Robot Hazard Analyzer");
+
+    }
 }
